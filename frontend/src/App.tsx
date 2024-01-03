b4f4950d8d3b7ea0ba9790cd54da93be314461c0
김진호 <rlawlsgh1227@gmail.com>
2024-01-04 01:41:08 +0900

    docs: 예시 구현 기능 목록 작성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import Main from './pages/Main';
+import Layout from '@/components/layout/Layout';
+import Main from '@/pages/Main';
 
 function App() {
   return (
