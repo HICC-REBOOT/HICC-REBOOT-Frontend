065a84d120a112a097e7dac26736c39f63c9013e
김진호 <rlawlsgh1227@gmail.com>
2024-01-07 15:29:32 +0900

    design: fontstyle 정의 중

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,5 +1,32 @@
 const colors = {};
-const fontStyle = {};
+
+// rem 10px: mobile font size
+const fontStyle = {
+  title4: `
+    font-family: Source Sans Pro;
+    font-weight: 700;
+    font-size: 4.8rem;  
+    letter-spacing: 0;
+  `,
+  title5: `
+    font-family: Source Sans Pro;
+    font-weight: 700;
+    font-size: 3.2rem;  
+    letter-spacing: 0;
+  `,
+  title6: `
+    font-family: Source Sans Pro;
+    font-weight: 700;
+    font-size: 2.4rem;  
+    letter-spacing: 0;
+  `,
+  title7: `
+    font-family: Source Sans Pro;
+    font-weight: 700;
+    font-size: 2rem;  
+    letter-spacing: 0;
+  `,
+};
 
 const theme = {
   colors,
