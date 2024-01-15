5bc364f7a1ca966a23152794b1863b6fd2174c02
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-01-15 17:36:21 +0900

    feat: 캘린더페이지 전체 레이아웃 구성

---
+++ frontend/src/pages/Calendar/Calendar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as C from './style/Calendar.style';
+
+export default function Calendar() {
+  return (
+    <C.Container>
+      <C.CalendarBox>temp Calendar</C.CalendarBox>
+      <C.DetailBox>detail info</C.DetailBox>
+    </C.Container>
+  );
+}
---
+++ frontend/src/pages/Calendar/style/Calendar.style.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding: 3.6rem 1.6rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.8rem;
+  background-color: ${(props) => props.theme.colors.black};
+`;
+
+export const CalendarBox = styled.div`
+  background-color: white;
+  border-radius: 1.6rem;
+  padding: 1.1rem;
+`;
+
+export const DetailBox = styled.div`
+  background-color: white;
+  border-radius: 1.6rem;
+  padding: 1.1rem;
+`;
