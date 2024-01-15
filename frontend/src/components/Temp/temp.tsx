146a2a9be0579b6dfab3900d6441d6909a5e7477
김진호 <rlawlsgh1227@gmail.com>
2024-01-15 16:46:09 +0900

    docs: 헤더 기능 목록 정의

--- frontend/src/components/Temp/temp.tsx
+++ frontend/src/components/Temp/temp.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Desktop, Mobile, Tablet } from '@assets/mediaQuery';
+import useInput from '@hooks/useInput';
 import * as T from './style/temp.style';
 
 function Temp() {
+  const [value, onChange] = useInput<string>('');
   return (
     <>
+      <input type="text" value={value} onChange={onChange} />
+
       <Mobile>
         <T.Text>Mobile</T.Text>
       </Mobile>
