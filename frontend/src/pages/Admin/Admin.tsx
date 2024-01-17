51d1bcdf816b1d5461cf06af3ce41d285d6e7ec3
Choi Seho <kindseho@naver.com>
2024-01-17 11:41:44 +0900

    feat : 상단 탭바 구현

--- frontend/src/pages/Admin/Admin.tsx
+++ frontend/src/pages/Admin/Admin.tsx
@@ -1,25 +1,65 @@
+/* eslint-disable no-use-before-define */
 import React from 'react';
-import * as A from './style/Admin.style';
+import styled from 'styled-components';
+import { Tabs, ConfigProvider } from 'antd';
+import type { TabsProps } from 'antd';
+import theme from '@styles/theme';
+import Approval from '@components/admin/Approval';
+
+const onChange = (key: string) => {
+  console.log(key);
+};
+
+const items: TabsProps['items'] = [
+  {
+    key: '1',
+    label: '회원 승인',
+    children: <Approval />,
+  },
+  {
+    key: '2',
+    label: '회원 정보',
+    children: 'Content of Tab Pane 2',
+  },
+  {
+    key: '3',
+    label: '등급 수정',
+    children: 'Content of Tab Pane 3',
+  },
+];
 
 function Admin() {
   return (
-    <A.Container>
-      <A.MembersBox>
-        <A.CategoryBox>
-          <A.MajorDivision>Major</A.MajorDivision>
-          <A.NameDivision>Name</A.NameDivision>
-          <A.StateDivision>State</A.StateDivision>
-        </A.CategoryBox>
-        <A.MemberBox>
-          <A.Major>캠퍼스자율전공(서울)</A.Major>
-          <A.Name>최세호</A.Name>
-          <A.State>
-            <A.StateText>대기</A.StateText>
-          </A.State>
-        </A.MemberBox>
-      </A.MembersBox>
-    </A.Container>
+    <Container>
+      <ConfigProvider
+        theme={{
+          components: {
+            Tabs: {
+              itemColor: theme.colors.white,
+              horizontalItemGutter: 110,
+            },
+          },
+        }}
+      >
+        <CustomTabs defaultActiveKey="1" items={items} onChange={onChange} animated={{ inkBar: true, tabPane: true }} />
+      </ConfigProvider>
+    </Container>
   );
 }
 
 export default Admin;
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding: 3.6rem 1.6rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.8rem;
+  background-color: ${(props) => props.theme.colors.black};
+`;
+
+const CustomTabs = styled(Tabs)`
+  color: ${theme.colors.white};
+`;
