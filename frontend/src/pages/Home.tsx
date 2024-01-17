8fcc94b28d0984739cf0bddac72e41cc1b3ad0de
JunhoKim <boy814@naver.com>
2024-01-18 02:16:25 +0900

    feat: add hongmap components for home screen page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import Activity from '@components/home/Activity';
 import ProjectList from '@components/home/ProjectList';
 import RecentNews from '@components/home/RecentNews';
+import HongMap from '@components/home/HongMap';
 
 const Container = styled.div``;
 
@@ -16,6 +17,7 @@ function Home() {
       <Activity />
       <ProjectList />
       <RecentNews />
+      <HongMap />
     </Container>
   );
 }
