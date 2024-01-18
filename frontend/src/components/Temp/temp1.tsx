ed79fd1a8fb26f447d81db6408479de526151ae8
김진호 <rlawlsgh1227@gmail.com>
2024-01-18 12:40:06 +0900

    feat: 드롭다운 기본적인 프레임 생성 및 열고 닫을 때 화살표 반전

--- frontend/src/components/Temp/temp1.tsx
+++ frontend/src/components/Temp/temp1.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Dropdown from '@components/common/dropdown/Dropdown';
+import OptionType from '@components/common/dropdown/OptionType';
 
 function Temp1() {
-  return <div style={{ height: '30rem' }}>temp1</div>;
+  const [option, setOption] = useState<OptionType | null>(null);
+
+  const options: OptionType[] = [
+    { value: '1', label: '등급 순' },
+    { value: '2', label: 'ho' },
+    { value: '3', label: 'yo' },
+    { value: '4', label: 'so' },
+  ];
+
+  const onChange = (selectedOption: OptionType | null) => {
+    setOption(selectedOption);
+  };
+
+  useEffect(() => {
+    console.log(option);
+  }, [option]);
+
+  return (
+    <div style={{ height: '30rem' }}>
+      <Dropdown placeholder="hello" options={options} onChange={onChange} />
+    </div>
+  );
 }
 
 export default Temp1;
---
+++ frontend/src/components/common/dropdown/Dropdown.style.tsx
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+import Select from 'react-select';
+import { DeviceProvider } from '@assets/mediaQuery';
+
+const DropdownStyle = styled(Select)`
+  & .Dropdown {
+    &__indicator-separator {
+      background-color: transparent;
+    }
+
+    &__dropdown-indicator {
+      padding: 0;
+    }
+
+    &__control {
+      ${(props) => props.theme.typography[DeviceProvider()].body};
+      width: 10.2rem;
+      height: 4.7rem;
+      background-color: ${(props) => props.theme.colors.black} !important;
+      border: none;
+
+      &--is-focused {
+        box-shadow: none;
+      }
+
+      border-radius: 0.8rem;
+      background-color: ${(props) => props.theme.colors.white};
+    }
+
+    &__value-container {
+      width: 100%;
+      height: 100%;
+      margin: 0;
+      padding: 0;
+      padding-left: 1.2rem;
+    }
+
+    &__placeholder {
+      margin: 0;
+      ${(props) => props.theme.typography[DeviceProvider()].body};
+    }
+
+    &__single-value {
+      color: ${(props) => props.theme.colors.white};
+      ${(props) => props.theme.typography[DeviceProvider()].body};
+    }
+
+    &__menu {
+      width: 10.2rem;
+      margin-top: 1.5rem;
+      border-top: 0;
+
+      &-list {
+        padding: 0;
+        justify-content: center;
+        align-items: center;
+      }
+    }
+
+    &__option {
+      height: 3rem;
+      display: flex;
+      align-items: center;
+      padding: 0.9rem 0 0.9rem 1.5rem;
+      border-top: 0.1rem solid ${(props) => props.theme.colors.grey001};
+      color: ${(props) => props.theme.colors.black};
+      background-color: ${(props) => props.theme.colors.white};
+
+      &--is-selected {
+      }
+
+      &--is-focused {
+      }
+    }
+  }
+`;
+
+export default DropdownStyle;
