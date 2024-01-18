36d92396e377b18261e369472378a1cdd26e1aca
Choi Seho <kindseho@naver.com>
2024-01-18 20:01:55 +0900

    feat: 회원 승인 대기에서 컨펌 적용

--- frontend/src/components/admin/ApprovalMemberItem.tsx
+++ frontend/src/components/admin/ApprovalMemberItem.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { log } from 'console';
+import Confirm from '@components/common/popup/confirm/Confirm';
 import * as A from './style/Approval.style';
 
 interface UserData {
@@ -9,14 +11,31 @@ interface UserData {
 interface MemberItemProps {
   userData: UserData;
 }
-
+const ClickAccept = () => {
+  console.log('클릭함');
+};
+const Ok = () => {
+  console.log('승인');
+};
+const Cancel = () => {
+  console.log('거절');
+};
 export default function ApprovalMemberItem({ userData }: MemberItemProps) {
   return (
     <A.MemberBox>
       <A.Major>{userData.major}</A.Major>
       <A.Name>{userData.name}</A.Name>
       <A.State>
-        <A.StateText>대기</A.StateText>
+        <A.StateText onClick={ClickAccept}>
+          {/* <Confirm
+            content={'대기'}
+            okText={'승인'}
+            cancelText={'거절'}
+            isDangerous={false}
+            onOk={Ok}
+            onCancel={Cancel}
+          /> */}
+        </A.StateText>
       </A.State>
     </A.MemberBox>
   );
