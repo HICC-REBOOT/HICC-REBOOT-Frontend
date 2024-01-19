62a853b1a30978276f42dab47f9397e4830df60a
김진호 <rlawlsgh1227@gmail.com>
2024-01-19 22:15:07 +0900

    chore: 충돌 재해결

--- frontend/src/components/Temp/style/temp.style.tsx
+++ frontend/src/components/Temp/style/temp.style.tsx
@@ -3,25 +3,6 @@ import { DeviceProvider } from '@assets/mediaQuery';
 
 export const Text = styled.div`
   ${(props) => props.theme.typography[DeviceProvider()].head};
-<<<<<<< HEAD
-=======
-
-  ${(props) => props.theme.media.mobile`
-    color: blue;
-  `}
-
-  ${(props) => props.theme.media.tablet`
-    color: grey;
-  `}
-
-  ${(props) => props.theme.media.desktop`
-    color: yellow;
-  `}
-
-  ${(props) => props.theme.media.wide`
-    color: green;
-  `}
->>>>>>> 9246afa2c751935318f7133383e4ccffd123ad36
 `;
 
 export const Temp = styled.div;
