bc2705b2e44a8c3c1724ef183881c9755a8f6cc3
김진호 <rlawlsgh1227@gmail.com>
2024-01-19 11:58:26 +0900

    design: 컨펌 내 추가로 선택할 수 있는 버튼 제작

---
+++ frontend/src/components/common/popup/confirm/ExtensionConfirm.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Modal } from 'antd';
+import './ExtensionConfirm.css';
+
+interface ConfirmParameter {
+  title: string;
+  content: React.JSX.Element;
+  okText: string;
+  cancelText: string;
+  isDangerous: boolean;
+  onOk: () => void;
+  onCancel?: () => void;
+}
+
+function extensionConfirm({ title, content, okText, cancelText, isDangerous, onOk, onCancel }: ConfirmParameter) {
+  Modal.confirm({
+    title,
+    content,
+    okText,
+    okButtonProps: { danger: isDangerous },
+    cancelText,
+    onOk() {
+      onOk();
+    },
+    onCancel() {
+      if (onCancel !== undefined) {
+        onCancel();
+      }
+    },
+  });
+}
+
+export default extensionConfirm;
