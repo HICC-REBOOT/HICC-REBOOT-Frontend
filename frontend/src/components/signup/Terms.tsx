0baf09073c72e504ea1dfbdf1e36feeff435028e
Choi Seho <kindseho@naver.com>
2024-01-22 17:43:40 +0900

    feat: 약관동의 안내사항 동의 화면 구성

--- frontend/src/components/signup/Terms.tsx
+++ frontend/src/components/signup/Terms.tsx
@@ -15,20 +15,48 @@ export default function Terms() {
         <T.ScrollBal>scroll</T.ScrollBal>
       </T.TermBox>
       <T.CheckerWrapper>
-        <T.Essential>필수</T.Essential>
+        <T.Essential>
+          <T.EssentialText>필수</T.EssentialText>
+        </T.Essential>
         <T.CheckText>이용약관에 동의합니다.</T.CheckText>
-        <T.CheckBox>체크박스</T.CheckBox>
+        <T.CheckBox>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path
+              d="M19 5V19H5V5H19ZM19 3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3Z"
+              fill="#C6C7CA"
+            />
+          </svg>
+        </T.CheckBox>
       </T.CheckerWrapper>
       <T.CheckerWrapper>
-        <T.Essential>필수</T.Essential>
+        <T.Essential>
+          <T.EssentialText>필수</T.EssentialText>
+        </T.Essential>
         <T.CheckText>이용약관에 동의합니다.</T.CheckText>
-        <T.CheckBox>체크박스</T.CheckBox>
+        <T.CheckBox>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path
+              d="M19 5V19H5V5H19ZM19 3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3Z"
+              fill="#C6C7CA"
+            />
+          </svg>
+        </T.CheckBox>
       </T.CheckerWrapper>
       <T.CheckerWrapper>
-        <T.Essential>필수</T.Essential>
+        <T.Essential>
+          <T.EssentialText>필수</T.EssentialText>
+        </T.Essential>
         <T.CheckText>이용약관에 동의합니다.</T.CheckText>
-        <T.CheckBox>체크박스</T.CheckBox>
+        <T.CheckBox>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path
+              d="M19 5V19H5V5H19ZM19 3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3Z"
+              fill="#C6C7CA"
+            />
+          </svg>
+        </T.CheckBox>
       </T.CheckerWrapper>
+      <T.CountinueButton>가입 진행하기</T.CountinueButton>
     </>
   );
 }
--- frontend/src/components/signup/style/Terms.style.tsx
+++ frontend/src/components/signup/style/Terms.style.tsx
@@ -1,12 +1,86 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
+import { DeviceProvider } from '@assets/mediaQuery';
 
-export const Title = styled.div``;
-export const TermBox = styled.div``;
-export const TextArea = styled.div``;
-export const ScrollBal = styled.div``;
-export const CheckerWrapper = styled.div``;
-export const Essential = styled.div``;
-export const CheckText = styled.div``;
-export const CheckBox = styled.div``;
-export const CountinueButton = styled.div``;
+export const Title = styled.div`
+  color: ${(props) => props.theme.colors.white};
+  text-align: center;
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  ${(props) => props.theme.typography[DeviceProvider()].body}
+`;
+export const TermBox = styled.div`
+  display: flex;
+  width: 32.8rem;
+  height: 10.9rem;
+  flex-shrink: 0;
+  border-radius: 1.6rem;
+  background: ${(props) => props.theme.colors.grey001};
+`;
+export const TextArea = styled.div`
+  width: 28.6rem;
+  height: 8.3046rem;
+  flex-shrink: 0;
+  color: ${(props) => props.theme.colors.white};
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  ${(props) => props.theme.typography.common.caption2}
+`;
+export const ScrollBal = styled.div`
+  width: 0.5rem;
+  height: 9.3rem;
+  flex-shrink: 0;
+  border-radius: 1rem;
+  background: rgba(255, 255, 255, 0.15);
+`;
+export const CheckerWrapper = styled.div`
+  display: flex;
+  width: 32.8rem;
+  height: 5.4rem;
+`;
+export const Essential = styled.div`
+  display: flex;
+  width: 3.3rem;
+  height: 2rem;
+  padding: 1rem;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  flex-shrink: 0;
+  border-radius: var(--Column, 0.4rem);
+  background: var(--point-110, rgba(91, 78, 245, 0.1));
+`;
+export const EssentialText = styled.div`
+  color: ${(props) => props.theme.colors.point1};
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  ${(props) => props.theme.typography.common.caption2}
+`;
+export const CheckText = styled.div`
+  width: 22.4rem;
+  color: ${(props) => props.theme.colors.white};
+  font-feature-settings:
+    'clig' off,
+    'liga' off;
+  ${(props) => props.theme.typography.common.caption1}
+`;
+export const CheckBox = styled.div`
+  width: 2.4rem;
+  height: 2.4rem;
+  flex-shrink: 0;
+`;
+export const CountinueButton = styled.div`
+  display: flex;
+  width: 12.6rem;
+  height: 4.6rem;
+  padding: 1rem 2rem;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  flex-shrink: 0;
+  border-radius: 1.2rem;
+  background: ${(props) => props.theme.colors.grey002};
+`;
