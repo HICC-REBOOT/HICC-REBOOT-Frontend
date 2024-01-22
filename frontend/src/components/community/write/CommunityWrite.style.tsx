b3320f996f2be73f6510a7b91ac2d3c095e4c222
김진호 <rlawlsgh1227@gmail.com>
2024-01-22 13:24:28 +0900

    feat: 이미지 업로드 기능 구현

--- frontend/src/components/community/write/CommunityWrite.style.tsx
+++ frontend/src/components/community/write/CommunityWrite.style.tsx
@@ -33,50 +33,6 @@ export const InputTitle = styled.input`
   }
 `;
 
-export const ImageContainer = styled.aside``;
-
-export const Label = styled.div`
-  margin-bottom: 1rem;
-  color: ${(props) => props.theme.colors.white};
-  ${(props) => props.theme.typography[DeviceProvider()].body};
-`;
-
-export const ImageUploads = styled.div`
-  display: flex;
-  width: 100%;
-  overflow-x: scroll;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-export const ImageUploadButton = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 6rem;
-  height: 6rem;
-  flex-shrink: 0;
-
-  border-radius: 1rem;
-  background: ${(props) => props.theme.colors.grey001};
-`;
-
-export const UploadButton = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 4.6rem;
-  padding: 1rem 2rem;
-
-  border: none;
-  border-radius: 0.8rem;
-  background: ${(props) => props.theme.colors.point1};
-
-  color: ${(props) => props.theme.colors.white};
-  ${(props) => props.theme.typography.common.button1};
-`;
-
 export const Textarea = styled.textarea`
   width: 100%;
   min-height: 32.5rem;
@@ -95,4 +51,61 @@ export const Textarea = styled.textarea`
   &::placeholder {
     color: ${(props) => props.theme.colors.grey002};
   }
+`;
+
+export const ImageContainer = styled.aside`
+  margin-bottom: 4rem;
+`;
+
+export const Label = styled.div`
+  margin-bottom: 1rem;
+  color: ${(props) => props.theme.colors.white};
+  ${(props) => props.theme.typography[DeviceProvider()].body};
+`;
+
+export const ImageUploads = styled.div`
+  display: flex;
+  width: 100%;
+  overflow-x: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const ImageView = styled.img`
+  width: 6rem;
+  height: 6rem;
+  flex-shrink: 0;
+  margin-right: 1rem;
+
+  border-radius: 1rem;
+  object-fit: cover;
+`;
+
+export const ImageUploadButton = styled.div<{ hide: boolean }>`
+  display: ${(props) => (props.hide ? 'none' : 'flex')};
+  justify-content: center;
+  align-items: center;
+  width: 6rem;
+  height: 6rem;
+  flex-shrink: 0;
+
+  border-radius: 1rem;
+  background: ${(props) => props.theme.colors.grey001};
+`;
+
+export const UploadButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4.6rem;
+  margin: 0 auto;
+  padding: 1rem 2rem;
+
+  border: none;
+  border-radius: 0.8rem;
+  background: ${(props) => props.theme.colors.point1};
+
+  color: ${(props) => props.theme.colors.white};
+  ${(props) => props.theme.typography.common.button1};
 `;
--- frontend/src/components/community/write/CommunityWrite.tsx
+++ frontend/src/components/community/write/CommunityWrite.tsx
@@ -1,11 +1,14 @@
-import React, { FormEventHandler } from 'react';
+import React, { FormEventHandler, useRef } from 'react';
 import { ReactComponent as ImageUpload } from '@assets/image/icon/ImageUpload.svg';
 import { useForm } from 'react-hook-form';
+import COMMON from '@constants/common';
+import getImageMetas from '@utils/image/getImageMeta';
 import * as W from './CommunityWrite.style';
+import EachImage from './EachImage';
 
 // 새로 등록은 file, 이미 있는 이미지면 src (수정을 위해)
 interface Image {
-  File?: File;
+  file?: File;
   src: string;
 }
 
@@ -16,7 +19,44 @@ interface WriteForm {
 }
 
 function CommunityWrite() {
-  const { handleSubmit, register } = useForm<WriteForm>();
+  const { handleSubmit, register, setValue, watch } = useForm<WriteForm>();
+  const fileRef = useRef<HTMLInputElement | null>(null);
+
+  // 현재 등록되어있는 이미지
+  const currentImages = watch('image');
+
+  // 이미지 버튼을 눌렀을 때 파일 선택 창을 띄우는 함수
+  const handleImgBtn = () => {
+    if (fileRef.current) {
+      fileRef.current.click();
+    }
+  };
+
+  // 이미지 등록 개수 초과검증 (추가 파일과 현재 등록돼있는 이미지가 10개 초과면 에러)
+  const checkOverImageLength = (fileLength: number) => {
+    const currentImageLength = currentImages === undefined ? 0 : currentImages.length;
+    const totalImageLength = fileLength + currentImageLength;
+
+    return totalImageLength > COMMON.IMAGE.MAX_LENGTH;
+  };
+
+  // 파일을 입력받아 state에 반영
+  const handleFileSelect = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+    if (files === null) return;
+
+    if (checkOverImageLength(files.length)) {
+      alert('이미지 등록 개수를 초과했습니다.');
+      return;
+    }
+
+    if (currentImages === undefined) {
+      setValue('image', await getImageMetas(files));
+      return;
+    }
+
+    setValue('image', [...Array.from(currentImages), ...(await getImageMetas(files))]);
+  };
 
   const textareaAutosize: FormEventHandler<HTMLTextAreaElement> = (e) => {
     const element = e.target as HTMLTextAreaElement;
@@ -38,14 +78,23 @@ function CommunityWrite() {
         })}
       />
       <W.ImageContainer>
+        <input
+          type="file"
+          style={{ display: 'none' }}
+          ref={fileRef}
+          onChange={handleFileSelect}
+          accept=".jpg,.png,.jpeg"
+          multiple
+        />
         <W.Label>사진 업로드</W.Label>
         <W.ImageUploads>
-          <W.ImageUploadButton>
+          {currentImages?.map((image, index) => <EachImage key={index} image={image.src} />)}
+          <W.ImageUploadButton hide={currentImages?.length >= COMMON.IMAGE.MAX_LENGTH} onClick={handleImgBtn}>
             <ImageUpload style={{ cursor: 'pointer' }} />
           </W.ImageUploadButton>
         </W.ImageUploads>
-        <W.UploadButton type="submit">업로드 하기</W.UploadButton>
       </W.ImageContainer>
+      <W.UploadButton type="submit">업로드 하기</W.UploadButton>
     </W.Container>
   );
 }
