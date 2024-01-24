f23e83efdd74bc4bfaf5d79812b2452c5f03e347
김진호 <rlawlsgh1227@gmail.com>
2024-01-24 11:35:08 +0900

    feat: 로그아웃 기능 구현

---
+++ frontend/src/auth/logout.ts
@@ -0,0 +1,15 @@
+import { TOKEN_KEYS } from '@constants/keys';
+import { removeCookie } from '@utils/cookie';
+import request from '@utils/request';
+
+async function logout() {
+  await request<null, null, null>({
+    uri: '/api/logout',
+    method: 'patch',
+  });
+
+  // 로그아웃 시 쿠키에서 리프레시토큰 삭제
+  removeCookie(TOKEN_KEYS.REFRESH_KEY);
+}
+
+export default logout;
