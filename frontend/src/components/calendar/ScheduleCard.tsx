99bc60c8c1206a77148edc4bd661afdcdbc46160
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-01-29 01:15:50 +0900

    feat: 캘린더 일정상세카드 설명숨기기 기능 추가

--- frontend/src/components/calendar/ScheduleCard.tsx
+++ frontend/src/components/calendar/ScheduleCard.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import theme from '@styles/theme';
-import { useRecoilState } from 'recoil';
-import { modalState } from '../../state/calendar';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { Variants } from 'framer-motion';
+import Switch from '@components/common/switch/Switch';
+import arrow from '@assets/image/icon/arrow.svg';
+import { isAdminState, modalState } from '../../state/calendar';
 import * as S from './style/ScheduleCard.style';
+
+const variants: Variants = {
+  notShow: {
+    transform: 'rotate(0deg)',
+    transition: { duration: 0.2 },
+  },
+  show: {
+    transform: 'rotate(180deg)',
+    transition: { duration: 0.2 },
+  },
+};
 
 export default function ScheduleCard() {
   const detail =
     '컴퓨터에 관심이 0.1이라도 있으신 분컴공과 학생(특히 들어오면 후회 안함;;컴잘알 형 많음)컴공으로 복전 생각 한 번이라도 하신 분너도 나도 배운다는 프로그래밍에 혹해본 적 있으신 분개임/앱개발 해보고 싶으신 분친구가 필요한 복학생ㅜ.ㅜ<활동내용>1. 매 주 동아리원이 자율적으로 주최&참여하는 세미나- c언어, java, 포토샵 등 상세 내용 및 일정은 다름- 소규모 그룹 과외식으로 진행되기 때문에 매우매우 고퀄리티 보장!- 맞춤형 커스터마이징 수업- 세미나 후 친목도모용 뒷풀이~(~.~)~';
 
-  const [isOpen, setIsOpen] = useRecoilState(modalState);
+  const [isModalOpen, setIsModalOpen] = useRecoilState(modalState);
+  const isAdmin = useRecoilValue(isAdminState);
+
+  const [isDetailOpen, setIsDetailOpen] = useState<boolean>(false);
+  const toggleDetail = () => {};
 
   return (
     <S.Container>
@@ -21,11 +39,21 @@ export default function ScheduleCard() {
         </S.TimeContainer>
         <S.InfoContainer>
           <S.Title>주간 세미나 일정</S.Title>
-          <S.Detail>상세 일정</S.Detail>
-          <S.Detail>{detail}</S.Detail>
+          <S.Detail isDetailOpen={isDetailOpen}>{detail}</S.Detail>
         </S.InfoContainer>
       </S.Left>
-      <S.EditBtn onClick={() => setIsOpen(true)} />
+      {isAdmin ? (
+        <S.Btn onClick={() => setIsModalOpen(true)} />
+      ) : (
+        <S.ArrowIcon
+          variants={variants}
+          alt="arrow"
+          src={arrow}
+          initial="notShow"
+          animate={isDetailOpen ? 'show' : 'notShow'}
+          onClick={() => setIsDetailOpen(!isDetailOpen)}
+        />
+      )}
     </S.Container>
   );
 }
--- frontend/src/components/calendar/style/ScheduleCard.style.tsx
+++ frontend/src/components/calendar/style/ScheduleCard.style.tsx
@@ -1,12 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { DeviceProvider } from '@assets/mediaQuery';
+import { motion } from 'framer-motion';
 
 export const Container = styled.div`
   border-radius: 1.6rem;
   background-color: #141415;
   display: flex;
   justify-content: space-between;
-  padding: 1.2rem 1.6rem;
+  padding: 0.8rem 1.2rem;
   width: 100%;
   position: relative;
 `;
@@ -14,7 +15,8 @@ export const Container = styled.div`
 export const Left = styled.div`
   display: flex;
   align-items: flex-start;
-  padding-right: 3.6rem;
+  padding-right: 4.8rem;
+  width: 100%;
 `;
 
 export const TimeContainer = styled.div`
@@ -37,7 +39,9 @@ export const Time2 = styled.span`
   opacity: 0.7;
 `;
 
-export const InfoContainer = styled.div``;
+export const InfoContainer = styled.div`
+  width: 100%;
+`;
 
 export const Title = styled.div`
   ${(props) => props.theme.typography[DeviceProvider()].body}
@@ -53,15 +57,20 @@ export const Title = styled.div`
   `};
 `;
 
-export const Detail = styled.div`
+export const Detail = styled.div<{ isDetailOpen: boolean }>`
   ${(props) => props.theme.typography.common.caption2};
   color: ${(props) => props.theme.colors.white};
   opacity: 0.7;
   text-align: justify;
 
-  ${(props) => props.theme.media.tablet`
-    max-width: 19rem;
-  `};
+  ${(props) =>
+    !props.isDetailOpen &&
+    css`
+      width: calc(100% - 6.4rem);
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    `}
 
   ${(props) => props.theme.media.desktop`
     max-width: 50rem;
@@ -72,7 +81,7 @@ export const Detail = styled.div`
   `};
 `;
 
-export const EditBtn = styled.button`
+export const Btn = styled.button`
   border: none;
   background-color: ${(props) => props.theme.colors.red};
   width: 2.4rem;
@@ -82,4 +91,15 @@ export const EditBtn = styled.button`
   position: absolute;
   right: 1.6rem;
   top: 2rem;
+`;
+
+export const ArrowIcon = styled(motion.img)`
+  width: 2.4rem;
+  height: 2.4rem;
+  flex-shrink: 0;
+  margin-left: 1.2rem;
+  position: absolute;
+  right: 1.6rem;
+  top: 2rem;
+  cursor: pointer;
 `;
