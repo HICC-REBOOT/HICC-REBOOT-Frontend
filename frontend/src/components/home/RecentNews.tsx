5e1a5393d4ff555fc0b1d3cd5c6d202e44ecbe81
JunhoKim <boy814@naver.com>
2024-01-29 17:30:03 +0900

    feat: 홈화면 통신 연결 코드 추가

--- frontend/src/components/home/RecentNews.tsx
+++ frontend/src/components/home/RecentNews.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
+import useGetLatestNews from '@query/get/useGetLatestNews';
 import * as R from '@components/home/style/RecentNews.style';
 
-const NEWS_ITEMS = [
-  {
-    title: '저희 엠티 안 가나요?',
-    label: '운영진',
-    writer: '장윤영3',
-    date: '2024-01-09 16:49',
-  },
-  {
-    title: '저희 엠티 안 가나요?',
-    label: '운영진',
-    writer: '장윤영3',
-    date: '2024-01-09 16:49',
-  },
-  {
-    title: '저희 엠티 안 가나요?',
-    label: '운영진',
-    writer: '장윤영3',
-    date: '2024-01-09 16:49',
-  },
-];
+// const NEWS_ITEMS = [
+//   {
+//     title: '저희 엠티 안 가나요?',
+//     label: '운영진',
+//     writer: '장윤영3',
+//     date: '2024-01-09 16:49',
+//   },
+//   {
+//     title: '저희 엠티 안 가나요?',
+//     label: '운영진',
+//     writer: '장윤영3',
+//     date: '2024-01-09 16:49',
+//   },
+//   {
+//     title: '저희 엠티 안 가나요?',
+//     label: '운영진',
+//     writer: '장윤영3',
+//     date: '2024-01-09 16:49',
+//   },
+// ];
 
 function RecentNews() {
+  const newsResponse = useGetLatestNews(); // Retrieve the entire response object
+  const NEWS_ITEMS = newsResponse.data;
+
   return (
     <R.Container>
       <R.Circle />
@@ -30,16 +34,17 @@ function RecentNews() {
         <R.Title>최근 소식</R.Title>
         <R.Content>더 많은 정보를 얻고싶다면 HICC에 가입해보세요</R.Content>
         <R.NewsList>
-          {NEWS_ITEMS.map((items, index) => (
-            <R.NewsBox key={index}>
-              <R.NewsTitle>{items.title}</R.NewsTitle>
-              <R.NewsBottom>
-                <R.Label>{items.label}</R.Label>
-                <R.Writer>{items.writer}</R.Writer>
-                <R.Date>{items.date}</R.Date>
-              </R.NewsBottom>
-            </R.NewsBox>
-          ))}
+          {NEWS_ITEMS &&
+            Object.values(NEWS_ITEMS).map((item: any, index: number) => (
+              <R.NewsBox key={index}>
+                <R.NewsTitle>{item.subject}</R.NewsTitle>
+                <R.NewsBottom>
+                  <R.Label>{item.grade}</R.Label>
+                  <R.Writer>{item.name}</R.Writer>
+                  <R.Date>{item.date}</R.Date>
+                </R.NewsBottom>
+              </R.NewsBox>
+            ))}
         </R.NewsList>
       </R.GroupContainer>
     </R.Container>
