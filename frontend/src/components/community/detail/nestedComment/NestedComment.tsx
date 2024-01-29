02dc17e3ba96403ba29577ce76893cbc52978db8
김진호 <rlawlsgh1227@gmail.com>
2024-01-29 14:30:32 +0900

    feat: 대댓글 삭제 연결

--- frontend/src/components/community/detail/nestedComment/NestedComment.tsx
+++ frontend/src/components/community/detail/nestedComment/NestedComment.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ReactComponent as KeyboardReturn } from '@assets/image/icon/keyboardReturn.svg';
 import WriteInfo from '@components/community/common/WriteInfo';
 import { NestedCommentType } from '@components/community/CommunityType';
+import useDeleteNestedComment from '@query/delete/useDeleteNestedComment';
 import * as N from './NestedComment.style';
 
 interface NestedCommentProps {
@@ -9,7 +10,16 @@ interface NestedCommentProps {
 }
 
 function NestedComment({ nestedComment }: NestedCommentProps) {
-  const deleteNestedComment = (commentId: number) => {};
+  const { deleteNestedComment, isPending } = useDeleteNestedComment({
+    articleId: nestedComment.articleId,
+    commentId: nestedComment.commentId,
+  });
+
+  const deleteThisNestedComment = () => {
+    if (window.confirm('정말 이 대댓글을 삭제하시겠습니까?')) {
+      deleteNestedComment();
+    }
+  };
 
   return (
     <N.Container>
@@ -17,7 +27,7 @@ function NestedComment({ nestedComment }: NestedCommentProps) {
       <N.Content>
         <WriteInfo name={nestedComment.name} grade={nestedComment.grade} date={nestedComment.date} />
         <N.P>{nestedComment.content}</N.P>
-        <N.DeleteButton isMine={nestedComment.isMine} onClick={() => deleteNestedComment(nestedComment.commentId)}>
+        <N.DeleteButton isMine={nestedComment.isMine} onClick={deleteThisNestedComment} disabled={isPending}>
           삭제
         </N.DeleteButton>
       </N.Content>
