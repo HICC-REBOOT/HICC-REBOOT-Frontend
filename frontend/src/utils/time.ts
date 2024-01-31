1cb12ce36912890b8e720ea575374a118e8a2331
김진호 <rlawlsgh1227@gmail.com>
2024-02-01 02:41:45 +0900

    fix: 시간함수 빈 문자열이면 빈 문자열 반환되도록 변경

--- frontend/src/utils/time.ts
+++ frontend/src/utils/time.ts
@@ -1,6 +1,10 @@
 import dayjs from 'dayjs';
 
 function time(stringTypeTime: string) {
+  if (stringTypeTime === '') {
+    return '';
+  }
+
   const koreanTime = dayjs(stringTypeTime);
   return koreanTime.format('YYYY-MM-DD HH:mm');
 }
