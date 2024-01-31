01cc46e9bdadc8d59cacf2b082f6194b6e064c5e
김진호 <rlawlsgh1227@gmail.com>
2024-01-31 23:32:54 +0900

    feat: 오류페이지 스타일 조정

--- frontend/src/components/common/error/GlobalError.style.tsx
+++ frontend/src/components/common/error/GlobalError.style.tsx
@@ -1,8 +1,5 @@
 import { DeviceProvider } from '@assets/mediaQuery';
 import styled from 'styled-components';
-import small404 from '@assets/image/404small.png';
-import big404 from '@assets/image/404big.png';
-import { Link } from 'react-router-dom';
 
 export const Container = styled.main`
   display: flex;
@@ -15,10 +12,10 @@ export const Container = styled.main`
 `;
 
 export const Oops = styled.header`
-  margin-top: 4.8rem;
+  margin-top: 16.1rem;
 
   ${(props) => props.theme.media.tablet`
-    margin-top: 5.1rem;
+    margin-top: 14.3rem;
   `};
 
   ${(props) => props.theme.media.desktop`
@@ -58,55 +55,22 @@ export const ErrorMessage = styled.div`
   white-space: pre-line;
 `;
 
-export const Image = styled.div`
-  width: 27.6rem;
-  height: 27.6rem;
-  margin: 1.8rem auto 0 auto;
-
-  background-image: url(${small404});
+// 스타일 조정
+export const NavigatePreButton = styled.button`
+  margin-top: 4.3rem;
 
   ${(props) => props.theme.media.tablet`
-    background-image: url(${big404});
-    width: 47rem;
-    height: 47rem;
+    margin-top: 4.6rem;
   `};
 
   ${(props) => props.theme.media.desktop`
-    background-image: url(${big404});
-    width: 47rem;
-    height: 47rem;
+    margin-top: 6.3rem;
   `};
 
   ${(props) => props.theme.media.wide`
-    background-image: url(${big404});
-    width: 47rem;
-    height: 47rem;
+    margin-top: 6.3rem;
   `};
 
-  background-repeat: no-repeat;
-  background-size: contain;
-`;
-
-export const Buttons = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 0 auto;
-`;
-
-// 스타일 조정
-export const GoHome = styled(Link)`
-  margin-right: 1rem;
-  padding: 1rem 2rem;
-  border-radius: 1.2rem;
-  border: 1px solid #53ffcb;
-  background: linear-gradient(95deg, rgba(91, 78, 245, 0.3) 0%, rgba(91, 78, 245, 0) 100%),
-    linear-gradient(275deg, rgba(83, 255, 203, 0.3) 0%, rgba(83, 255, 203, 0) 100%);
-
-  color: ${(props) => props.theme.colors.white};
-  ${(props) => props.theme.typography.common.button1};
-`;
-
-export const RetryButton = styled.button`
   padding: 1rem 2rem;
   border-radius: 1.2rem;
   border: 1px solid #53ffcb;
