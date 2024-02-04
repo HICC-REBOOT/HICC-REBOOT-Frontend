ff6923ea3ce070365088dbd959b24f039627eee5
JunhoKim <boy814@naver.com>
2024-02-05 01:59:24 +0800

    fix: 게시글 수정을 위해 게시글 자세 정보를 불러오는 코드 추가, 서버 종료 문제로 test는 못 해봄

--- frontend/src/components/mypage/MyWritings.tsx
+++ frontend/src/components/mypage/MyWritings.tsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ROUTE from '@constants/route';
 import { useNavigate } from 'react-router-dom';
 import COMMON from '@constants/common';
 import useServerSidePagination from '@query/get/useServerSidePagination';
 import { ArticleDetailType } from '@components/community/CommunityType';
+import fetchArticleDetail from '@query/get/useGetArticleDetail';
 
 import * as C from '@components/mypage/style/MyPageComponent.style';
 import * as M from './style/MyWritings.style';
 import DeleteButton from './button/ArticleDeleteButton';
+
+async function updateArticle(data: ArticleDetailType, navigate: any) {
+  try {
+    const DetailData = await fetchArticleDetail({ articleId: data.articleId });
+    navigate(`${ROUTE.COMMUNITY.BASE}/${ROUTE.COMMUNITY.UPDATE}`, { state: DetailData });
+  } catch (error) {
+    // Handle error if needed
+    console.error('Failed to fetch article details', error);
+  }
+}
 
 function MyWritings() {
   const { curPageItem, renderPaginationBtnOrInfinityScroll } = useServerSidePagination<ArticleDetailType>({
@@ -21,19 +32,15 @@ function MyWritings() {
     navigate(`${ROUTE.COMMUNITY.BASE}/${article.articleId}`);
   };
 
-  const updateArticle = (data: ArticleDetailType) => {
-    navigate(`${ROUTE.COMMUNITY.BASE}/${ROUTE.COMMUNITY.UPDATE}`, { state: data });
-  };
-
   return (
     <M.Container>
       <M.GroupContainer>
         <M.ContentsContainer>
           {Object.values(curPageItem).map((item: ArticleDetailType, index) => (
-            <M.ContentsArea key={index} onClick={() => goDetail(item)}>
-              <M.ContentsTitle>{item.subject}</M.ContentsTitle>
-              <M.Contents>{item.content}</M.Contents>
-              <M.ModifyButton onClick={() => updateArticle(item)}>수정</M.ModifyButton>
+            <M.ContentsArea key={index}>
+              <M.ContentsTitle onClick={() => goDetail(item)}>{item.subject}</M.ContentsTitle>
+              <M.Contents onClick={() => goDetail(item)}>{item.content}</M.Contents>
+              <M.ModifyButton onClick={() => updateArticle(item, navigate)}>수정</M.ModifyButton>
               <DeleteButton id={item.articleId}></DeleteButton>
               <M.Divider />
             </M.ContentsArea>
