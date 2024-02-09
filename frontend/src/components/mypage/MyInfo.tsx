2a3a6a6c4930c1697274059c25ad4aec20813e0b
Junho Kim <boy814@naver.com>
2024-02-09 17:46:28 +0900

    chore: 미사용 코드 삭제

--- frontend/src/components/mypage/MyInfo.tsx
+++ frontend/src/components/mypage/MyInfo.tsx
@@ -17,46 +17,6 @@ interface MajorInfo {
 }
 
 function MyInfo() {
-  // const options: OptionType[] = [
-  //   { value: '1', label: '기초과학과' },
-  //   { value: '2', label: '건설환경공학과' },
-  //   { value: '3', label: '전자전기공학부' },
-  //   { value: '4', label: '컴퓨터공학과' },
-  //   { value: '5', label: '산업·데이터공학과' },
-  //   { value: '6', label: '신소재화공시스템공학부' },
-  //   { value: '7', label: '기계·시스템디자인공학부' },
-  //   { value: '8', label: '건축학부' },
-  //   { value: '9', label: '도시공학과' },
-  //   { value: '10', label: '경영학부' },
-  //   { value: '11', label: '영어영문학과' },
-  //   { value: '12', label: '독어독문학과' },
-  //   { value: '13', label: '독어독문학과' },
-  //   { value: '14', label: '불어불문학과' },
-  //   { value: '15', label: '국어국문학과' },
-  //   { value: '16', label: '법학부' },
-  //   { value: '17', label: '수학교육과' },
-  //   { value: '18', label: '국어교육과' },
-  //   { value: '19', label: '영어교육과' },
-  //   { value: '20', label: '역사교육과' },
-  //   { value: '21', label: '교육학과' },
-  //   { value: '22', label: '동양화과' },
-  //   { value: '23', label: '회화과' },
-  //   { value: '24', label: '판화과' },
-  //   { value: '25', label: '조소과' },
-  //   { value: '26', label: '목조형가구학과' },
-  //   { value: '27', label: '예술학과' },
-  //   { value: '28', label: '금속조형디자인과' },
-  //   { value: '29', label: '도예유리과' },
-  //   { value: '30', label: '섬유미술패션디자인과' },
-  //   { value: '31', label: '자율전공' },
-  //   { value: '32', label: '디자인학부' },
-  //   { value: '33', label: '경제학부' },
-  //   { value: '34', label: '공연예술학부' },
-  //   { value: '35', label: '디자인경영융합학부' },
-  //   { value: '36', label: '캠퍼스자율전공(서울)' },
-  //   { value: '37', label: '교양교육원' },
-  // ];
-
   const majorsData = useGetMajors().data;
   const majorsList: MajorInfo[] = Array.isArray(majorsData) ? majorsData : [];
 
