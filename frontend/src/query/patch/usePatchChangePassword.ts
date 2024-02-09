217b5e0eadf8a0fa83b1b064144d508af8968e75
김진호 <rlawlsgh1227@gmail.com>
2024-02-09 22:23:46 +0900

    fix: 비밀번호 변경 알럿 및 메인으로 보내기 기능

--- frontend/src/query/patch/usePatchChangePassword.ts
+++ frontend/src/query/patch/usePatchChangePassword.ts
@@ -1,6 +1,7 @@
 import { QUERY_KEYS } from '@constants/keys';
 import { useMutation } from '@tanstack/react-query';
 import request from '@utils/request';
+import { useNavigate } from 'react-router-dom';
 
 export interface ChangePasswordRequest {
   nonce: string;
@@ -22,10 +23,15 @@ function usePatchChangePassword() {
     });
   };
 
+  const navigate = useNavigate();
+
   const { mutate, isPending } = useMutation({
     mutationKey: [QUERY_KEYS.PATCH_CHANGE_PASSWORD],
     mutationFn: requestChangePassword,
-    onSuccess: () => {},
+    onSuccess: () => {
+      alert('성공적으로 비밀번호가 변경되었습니다.');
+      navigate('/');
+    },
   });
 
   return {
