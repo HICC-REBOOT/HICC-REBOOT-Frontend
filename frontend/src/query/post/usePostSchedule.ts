18f30b81e37dcd7f9f13685d4620a962a3970b7e
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-02-09 11:52:41 +0900

    feat: 캘린더 일정 추가 api 연결

---
+++ frontend/src/query/post/usePostSchedule.ts
@@ -0,0 +1,59 @@
+import { ScheduleType } from '@components/calendar/CalendarType';
+import { QUERY_KEYS } from '@constants/keys';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import request from '@utils/request';
+
+interface usePostScheduleProps {
+  name: string;
+  startDateTime: string;
+  endDateTime: string;
+  type: ScheduleType;
+  content: string;
+}
+
+interface postScheduleRequestBody {
+  name: string;
+  startDateTime: string;
+  endDateTime: string;
+  type: ScheduleType;
+  content: string;
+}
+
+function usePostSchedule({ name, startDateTime, endDateTime, type, content }: usePostScheduleProps) {
+  const postSchedule = async () => {
+    const data: postScheduleRequestBody = {
+      name,
+      startDateTime,
+      endDateTime,
+      type,
+      content,
+    };
+
+    await request<postScheduleRequestBody, null, null>({
+      uri: '/api/calendar/schedule',
+      method: 'post',
+      data,
+    });
+
+    return true;
+  };
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationKey: [QUERY_KEYS.POST_SCHEDULE, { name, startDateTime, endDateTime, type, content }],
+    mutationFn: postSchedule,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.POST_SCHEDULE, { name, startDateTime, endDateTime, type, content }],
+      });
+    },
+  });
+
+  return {
+    postSchedule: mutate,
+    isPending,
+  };
+}
+
+export default usePostSchedule;
