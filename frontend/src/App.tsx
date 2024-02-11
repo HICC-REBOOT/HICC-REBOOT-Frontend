a8b7b10b2bed1ca20c4c802a0a0216d65151df74
김진호 <rlawlsgh1227@gmail.com>
2024-02-11 16:26:00 +0900

    fix: react-helmet 제거, og태그가 정상적으로 적용되지 않아서 다시 원복

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import { Helmet } from 'react-helmet-async';
-import calculateFoundationYear from '@utils/calculateFoundationYear';
 
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Layout from '@components/layout/Layout';
@@ -27,60 +25,46 @@ function App() {
     window.scrollTo({ top: 0 });
   }, [location.pathname]);
 
-  const fillFoundationYearDescription = () => {
-    return `${calculateFoundationYear()}년의 전통의 역사를 가진 홍익 콤퓨타연구반 HICC입니다.`;
-  };
-
   return (
-    <>
-      <Helmet>
-        <meta property="og:url" content="https://www.hicc.co.kr" />
-        <meta property="og:title" content="HICC" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="%PUBLIC_URL%/king_president.jpg" />
-        <meta property="og:description" content={fillFoundationYearDescription()} />
-        <title>Hong Ik Computer Club</title>
-      </Helmet>
-      <Routes>
-        <Route element={<Layout />}>
-          {/* admin */}
-          <Route path={ROUTE.ADMIN.BASE} element={<Admin />}>
-            <Route path={ROUTE.ADMIN.APPROVAL} element={<Approval />} />
-            <Route path={ROUTE.ADMIN.INFO} element={<MemberInfo />} />
-            <Route path={ROUTE.ADMIN.CHANGE} element={<ChangeGrade />} />
-          </Route>
-          <Route path={ROUTE.HOME} element={<Home />} />
-          <Route path={ROUTE.CALENDAR} element={<Calendar />} />
-          {/* auth */}
-          <Route path={ROUTE.SIGNUP} element={<SignUp />} />
-          <Route path={ROUTE.ENTERINFO} element={<EnterInfo />} />
-          <Route path={ROUTE.LOGIN} element={<Login />} />
-          <Route path={ROUTE.RESET_PASSWORD} element={<ResetPassword />} />
-          <Route path={ROUTE.RESET_PASSWORD_AFTER_EMAIL_VERIFY} element={<ChangePassword />} />
-          {/* community */}
-          <Route path={ROUTE.COMMUNITY.BASE} element={<Community />}>
-            <Route path={ROUTE.COMMUNITY.LIST} element={<CommunityList />} />
-            <Route path={ROUTE.COMMUNITY.WRITE} element={<CommunityWrite />} />
-            <Route path={ROUTE.COMMUNITY.DETAIL} element={<ArticleDetail />} />
-            <Route path={ROUTE.COMMUNITY.UPDATE} element={<CommunityUpdate />} />
-            <Route path={ROUTE.COMMUNITY.NOT_FOUND} element={<NotFound />} />
-          </Route>
-          <Route path={ROUTE.PROFILE.MYINFO} element={<MyInfomation />} />
-          {/* mypage */}
-          <Route path={ROUTE.PROFILE.BASE} element={<MyPage />}>
-            <Route path={ROUTE.PROFILE.MYWRITINGS} element={<MyWritings />} />
-            <Route path={ROUTE.PROFILE.MYCOMMENTS} element={<MyComments />} />
-          </Route>
-          {/* dev */}
-          <Route path={ROUTE.TEMP.BASE} element={<Temp />}>
-            <Route path={ROUTE.TEMP.APPLE} element={<Temp1 />} />
-            <Route path={ROUTE.TEMP.BANANA} element={<Temp2 />} />
-            <Route path={ROUTE.TEMP.MANGO} element={<Temp3 />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
+    <Routes>
+      <Route element={<Layout />}>
+        {/* admin */}
+        <Route path={ROUTE.ADMIN.BASE} element={<Admin />}>
+          <Route path={ROUTE.ADMIN.APPROVAL} element={<Approval />} />
+          <Route path={ROUTE.ADMIN.INFO} element={<MemberInfo />} />
+          <Route path={ROUTE.ADMIN.CHANGE} element={<ChangeGrade />} />
         </Route>
-      </Routes>
-    </>
+        <Route path={ROUTE.HOME} element={<Home />} />
+        <Route path={ROUTE.CALENDAR} element={<Calendar />} />
+        {/* auth */}
+        <Route path={ROUTE.SIGNUP} element={<SignUp />} />
+        <Route path={ROUTE.ENTERINFO} element={<EnterInfo />} />
+        <Route path={ROUTE.LOGIN} element={<Login />} />
+        <Route path={ROUTE.RESET_PASSWORD} element={<ResetPassword />} />
+        <Route path={ROUTE.RESET_PASSWORD_AFTER_EMAIL_VERIFY} element={<ChangePassword />} />
+        {/* community */}
+        <Route path={ROUTE.COMMUNITY.BASE} element={<Community />}>
+          <Route path={ROUTE.COMMUNITY.LIST} element={<CommunityList />} />
+          <Route path={ROUTE.COMMUNITY.WRITE} element={<CommunityWrite />} />
+          <Route path={ROUTE.COMMUNITY.DETAIL} element={<ArticleDetail />} />
+          <Route path={ROUTE.COMMUNITY.UPDATE} element={<CommunityUpdate />} />
+          <Route path={ROUTE.COMMUNITY.NOT_FOUND} element={<NotFound />} />
+        </Route>
+        <Route path={ROUTE.PROFILE.MYINFO} element={<MyInfomation />} />
+        {/* mypage */}
+        <Route path={ROUTE.PROFILE.BASE} element={<MyPage />}>
+          <Route path={ROUTE.PROFILE.MYWRITINGS} element={<MyWritings />} />
+          <Route path={ROUTE.PROFILE.MYCOMMENTS} element={<MyComments />} />
+        </Route>
+        {/* dev */}
+        <Route path={ROUTE.TEMP.BASE} element={<Temp />}>
+          <Route path={ROUTE.TEMP.APPLE} element={<Temp1 />} />
+          <Route path={ROUTE.TEMP.BANANA} element={<Temp2 />} />
+          <Route path={ROUTE.TEMP.MANGO} element={<Temp3 />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
