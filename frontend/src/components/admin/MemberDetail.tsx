d6cf2e05aac24bf37007222610f798a2c5fbb6b9
Choi Seho <kindseho@naver.com>
2024-02-19 23:14:18 +0900

    fix: 등급 영어에서 한글로 수정

--- frontend/src/components/admin/MemberDetail.tsx
+++ frontend/src/components/admin/MemberDetail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as M from './style/MemberDetail';
 
 export interface UserData {
@@ -16,9 +16,21 @@ interface MemberDetailProps {
 }
 
 export default function MemberDetail({ userData }: MemberDetailProps) {
+  const [grade, setGrade] = useState('');
+  useEffect(() => {
+    if (userData.grade === 'PRESIDENT') {
+      setGrade('회장');
+    } else if (userData.grade === 'EXECUTIVE') {
+      setGrade('운영진');
+    } else if (userData.grade === 'NORMAL') {
+      setGrade('회원');
+    } else {
+      setGrade('승인대기자');
+    }
+  }, [userData.grade]);
   return (
     <M.DetailBox>
-      <M.Class>등급: {userData.grade}</M.Class>
+      <M.Class>등급: {grade}</M.Class>
       <M.Major>학과: {userData.department}</M.Major>
       <M.Grade>학번: {userData.studentNumber}</M.Grade>
       <M.Phone>연락처: {userData.phoneNumber}</M.Phone>
