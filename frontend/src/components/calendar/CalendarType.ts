434acc0fd3c8f47707fc28a40dfa15a88d288f8e
김진호 <rlawlsgh1227@gmail.com>
2024-08-22 21:11:25 +0900

    fix: 타입오류 수정

--- frontend/src/components/calendar/CalendarType.ts
+++ frontend/src/components/calendar/CalendarType.ts
@@ -1,3 +1,10 @@
+type ACADEMIC = 'ACADEMIC';
+type AMITY = 'AMITY';
+type SCHOOL_EVENT = 'SCHOOL_EVENT';
+type ETC = 'ETC';
+
+export type ScheduleType = ACADEMIC | AMITY | SCHOOL_EVENT | ETC;
+
 // 월별 일정 조회
 export interface MonthSchedule {
   name: string;
@@ -18,9 +25,3 @@ export interface DaySchedule {
   startDateTime: string;
   endDateTime: string;
 }
-
-export type ScheduleType = ACADEMIC | AMITY | SCHOOL_EVENT | ETC;
-type ACADEMIC = 'ACADEMIC';
-type AMITY = 'AMITY';
-type SCHOOL_EVENT = 'SCHOOL_EVENT';
-type ETC = 'ETC';
