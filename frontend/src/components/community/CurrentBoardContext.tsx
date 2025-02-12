bae8b7f772d51131d23ab68325bcedc6c6dcb5ff
김진호 <rlawlsgh1227@gmail.com>
2025-02-12 20:54:14 +0900

    feat: 게시글 조회 게시판 변경으로 인한 수정

--- frontend/src/components/community/CurrentBoardContext.tsx
+++ frontend/src/components/community/CurrentBoardContext.tsx
@@ -1,7 +1,9 @@
 import OptionType from '@components/common/dropdown/OptionType';
+import { Board } from '@components/type/CommonType';
 
 interface CurrentBoardContext {
   currentBoard: OptionType;
+  boardList: Board[];
 }
 
 export default CurrentBoardContext;
